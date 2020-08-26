a3c9261f14a7e72adf4c6b5dc0aeb2507774c411
Behnam <gbehnam.bb@hotmail.com>
2020-08-27 00:03:02 +0200

    Create shopping-list.js

---
+++ freecodecamp/shopping-list.js
@@ -0,0 +1,6 @@
+var myList = [];
+myList.push(["roasted onion", 2.1]);
+myList.push(["Ice Cream", 4.3]);
+myList.push(["Chocolate", 2.5]);
+myList.push(["Vegetables", 1.95]);
+myList.push(["Feigen", 3]);
