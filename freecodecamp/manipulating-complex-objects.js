1135c9de93061f82daf5e5daee9615d45a09a713
Behnam <gbehnam.bb@hotmail.com>
2020-08-31 16:10:09 +0200

    Create manipulating-complex-objects.js

---
+++ freecodecamp/manipulating-complex-objects.js
@@ -0,0 +1,23 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add a record here
+  {
+    "artist": "Adele",
+    "title": "Truning Tables",
+    "release_year": 2010,
+    "formats": [
+      "MVM",
+      "MP3"
+    ]
+  }
+];
