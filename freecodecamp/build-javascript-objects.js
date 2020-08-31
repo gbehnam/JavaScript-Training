f34085d31c905dd29528fa3c63b84c5c328c76ad
Behnam <gbehnam.bb@hotmail.com>
2020-08-31 16:04:40 +0200

    Create build-javascript-objects.js

---
+++ freecodecamp/build-javascript-objects.js
@@ -0,0 +1,10 @@
+var myDog = {
+// Only change code below this line
+  name: "doggy",
+  legs: 4,
+  tails: 1,
+  friends: ["trooper", "golden retriever"]
+
+
+// Only change code above this line
+};
